710bfe5850a0790d0c4279119d9a5a69b586b604
Wayne Gordon Jr <wgordon2@albany.edu>
2022-07-13 21:22:36 -0400

    Update Mastercard.py

--- Mastercard.py
+++ Mastercard.py
@@ -5,17 +5,19 @@ def isAnagram(s: str, t: str) -> bool:
     if letter in dict1:
       dict1[letter]+=1
     else:
-      dict1[letter] =1
+      dict1[letter]=1
 
   for letter in t:
     if letter in dict2:
       dict2[letter]+=1
     else:
-      dict2[letter] =1
+      dict2[letter]=1
   print(dict1)
   print(dict2)
+  print("Are {} and {} Anagrams?".format(s,t))
   return dict1 == dict2
 
-s = "anagram"
-t = "nagaram"
+s,t = input("Enter two words").split()
+print("s is:", s)
+print("t is:", t)
 print(isAnagram(s,t))
